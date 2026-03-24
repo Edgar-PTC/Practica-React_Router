e3181a2775c4523b2f04406c20e1d5dab10ce268
Edgar-PTC <20230280@ricaldone.edu.sv>
2026-03-24 10:02:29 -0600

    ux:About Me page 30% ready

--- Navegacion/src/pages/AboutMe.jsx
+++ Navegacion/src/pages/AboutMe.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import Card from "../components/Cards.jsx";
 
 const AboutMe = () => {
+    return(
+        <>
+            <main className="perfil">
+                <div className="me">
+                    <h2>TYUO</h2>
+                </div>
+                <div className="gustos">
+                    <h2>drtvybjjjjjjjjjj</h2>
+                </div>
+            </main>
+            <div className="jjk">
+                <div className="Hechiceros">
+                    <img src="https://res.cloudinary.com/dvtk6ky3t/image/upload/v1774365384/Gojo_Satoru___Jujutsu_Kaisen_apct2h.jpg" alt="El hechicero mas fuerte de la actualidad" />
+                </div>
+                <div className="Hechiceros">
+                    <img src="https://res.cloudinary.com/dvtk6ky3t/image/upload/v1774365384/Ryomen_Sukuna_ymrcuh.jpg" alt="el hechicero mas fuerte de la historia" />
+                </div>
+            </div>
 
+        </>
+    )
 }
 
 export default AboutMe;
--- Navegacion/src/pages/Home.jsx
+++ Navegacion/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../components/Cards.jsx";
-import { cuartetoCanciones, brunoCanciones, moratCanciones } from "../Canciones";
+import { cuartetoCanciones, brunoCanciones, moratCanciones } from "../datos/Canciones";
 
 const Home = () => {
     return(
